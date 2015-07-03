a1ae699fa2b9d06606fd922fabdb1f1ee0540018
gueltro <gueltro@gmail.com>
2015-07-02 19:29:00 -0700

    added interactive setup for PGP keys

--- gpg_utils.py
+++ gpg_utils.py
@@ -1,20 +1,20 @@
 import gnupg
 from os.path import expanduser
 
-
-def gpg_setup():
-    home = expanduser("~")
+def gpg_setup(home = expanduser("~") + "/.alcos" ):
     gpg = gnupg.GPG(gnupghome=home)
     gpg.encoding = 'utf-8'
     return gpg
 
+##Import new gpg_keys
+def import_gpg_keys(gpg,data):
+    gpg.import_keys(data)
 
-    input_data = gpg.gen_key_input(key_type="RSA", key_length=1024)
-    key = gpg.gen_key(input_data)
-    ascii_armored_public_keys = gpg.export_keys(keyids)
-
-setup()
-
+##Interactivley create a gpg_identity
+def create_gpg_keys(gpg, params):
+   key_params = gpg.gen_key_input(**params) 
+#    key_params = gpg.gen_key_input(name_real="giulio", key_type="RSA", key_length=1024)
+   gpg.gen_key(key_params)
 
 
 ##A signer can use this to sign a message with his
@@ -24,12 +24,41 @@ def sign(message, signer_id, signer_gpg):
     print message + " signed with my private key: " + private_key
     return signed_data
     
-
-
 def verify(promise, promise_signature, verifier_gpg, original_owner):
     print "---"
     print promise
     print promise_signature
     print original_owner
     return verifier_gpg.verify(promise_signature)
+
+##Utils for interactive setup. Maybe in the future this will be done with the cli-interface
+def gpg_interactive_setup():
+    print "Welcome to the interactive gpg setup! Do you want to create a new PGP key-pair, or do you want to import one?"
+    user_input = "n"
+    while (user_input != "I") and (user_input!= "N"):
+        user_input = raw_input("N = New key-pair, I = Import an existing key-pair:   ") 
+ 
+    gpg = gpg_setup()      
+    if (user_input == "N"):
+        keys_interactive_setup(gpg)
+    
+    return gpg
+
+##Create a new PGP key from user input
+def keys_interactive_setup(new_gpg):
+    name_real = ""
+    while (name_real == ""):
+        name_real  = raw_input("Please insert your name (this will be used as a human readable identifier for your PGP identity):  ")
+   
+    while (name_real == ""):
+        name_real  = raw_input("Please insert your name (this will be used as a human readable identifier for your PGP identity):  ")
+
+    ##Prepare a list with all the parameter of the new PGP key
+    new_identity = {}
+    new_identity['name_real'] = name_real
+    new_identity['key_type'] = "RSA"
+    new_identity["key_length"] = "1024"
+    
+    create_gpg_keys(new_gpg, new_identity)
+     
 
